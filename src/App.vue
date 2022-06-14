9b4776ca94d30939c1ff62f76097c3e5b15fc735
jiwlee523 <ddaezi9576@ex-em.com>
2022-06-14 17:02:14 +0900

    TheHeader component 생성

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <h1>Vue App</h1>
+  <the-header :title="'Todo Calendar'"></the-header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import TheHeader from "@/components/TheHeader.vue";
 
 export default defineComponent({
   name: 'App',
+  components: {TheHeader},
 });
 </script>
 
