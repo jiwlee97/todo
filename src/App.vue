fd2731c3801a87fa3115c82fc7f07a8bd6eafd83
jiwlee523 <ddaezi9576@ex-em.com>
2022-06-14 16:46:27 +0900

    Init Project with Vue CLI

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Vue App</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
