b3e7f8353163d5f511cf5698b8f3124c54e16624
jiwlee <53519832+jiwlee97@users.noreply.github.com>
2022-06-14 21:48:22 +0900

    TheHeader 구현 (#3)
    * TheHeader에서 사용할 logo.png 파일 추가
    
    * TheHeader component 생성
    
    * favicon.ico 추가
    
    * *과 header padding, margin 수정
    
    Co-authored-by: jiwlee523 <ddaezi9576@ex-em.com>
    Co-authored-by: jiwlee <ddazi9576@google.com>

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <h1>Vue App</h1>
+  <the-header :title="'Todo Calendar'"></the-header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import TheHeader from "@/components/TheHeader.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
+  components: { TheHeader },
 });
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <img src="logo.png" class="logo-img" />
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TheHeader",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+header {
+  background-color: rgb(253 224 71);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+}
+.logo-img {
+  width: 24px;
+  height: 24px;
+  transform: rotate(-40deg);
+}
+h1 {
+  margin-left: 10px;
+  font-size: 20px;
+}
+</style>
